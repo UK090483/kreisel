9d6f7e71affc8f3aff5eb11d6a72014d4857d8b2
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-01-04 13:02:50 +0100

    init

--- services/googleService/client.tsx
+++ services/googleService/client.tsx
@@ -1,21 +1,23 @@
-import { google } from "googleapis";
+// import { google } from "googleapis";
 
-import secretKey from "../../serviceAccounts/googleServiceAccount.json";
+// import secretKey from "../../serviceAccounts/googleServiceAccount.json";
 
-let jwtClient = new google.auth.JWT(
-  secretKey.client_email,
-  undefined,
-  secretKey.private_key,
-  ["https://www.googleapis.com/auth/spreadsheets"]
-);
+// let jwtClient = new google.auth.JWT(
+//   secretKey.client_email,
+//   undefined,
+//   secretKey.private_key,
+//   ["https://www.googleapis.com/auth/spreadsheets"]
+// );
 
-jwtClient.authorize(function (err, tokens) {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log("Successfully connected!");
-  }
-});
+// jwtClient.authorize(function (err, tokens) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   } else {
+//     console.log("Successfully connected!");
+//   }
+// });
 
-export { jwtClient };
+// export { jwtClient };
+
+export {};
