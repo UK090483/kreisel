597ebff0f8a1802baeb3a2b0924fd07b35a7bcda
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-11-16 12:49:52 +0100

    change: disallow robots

--- components/Layout/Head/Head.tsx
+++ components/Layout/Head/Head.tsx
@@ -13,6 +13,8 @@ const Head: React.FunctionComponent<HeadProps> = ({ name }) => {
         name="description"
         content="Generated width love by create next app"
       />
+      <meta name="robots" content="noindex"></meta>
+      <meta name="robots" content="nofollow"></meta>
       <link rel="icon" href="/favicon-32x32.png" />
     </NextHead>
   );
