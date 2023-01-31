8a0cf8fb49af2af49a7893f032a33532073d7eaa
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2023-01-31 11:03:08 +0100

    adding env typings

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    EMAIL_SERVER_USER: string;
+    EMAIL_SERVER_PASSWORD: string;
+    EMAIL_SERVER_HOST: string;
+    EMAIL_SERVER_PORT: number;
+    EMAIL_FROM: string;
+  }
+}
