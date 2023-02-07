222486e3b25c8cae90572b0e9588763d181ac0e4
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2023-02-07 09:26:00 +0100

    run

--- cypress/e2e/pages.cy.ts
+++ cypress/e2e/pages.cy.ts
@@ -2,7 +2,7 @@ describe("pages", () => {
   Cypress.env("pages").forEach((e) => {
     it(`should have page: ${e.slug}`, () => {
       if (e.slug) {
-        cy.visit(e.slug, { timeout: 5000 });
+        cy.visit(e.slug);
       }
     });
   });
