60510eeb0addc92934179c2d01edd761dab933cb
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2023-02-18 19:36:08 +0100

    c

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "cypress";
 
-// import { createClient } from "@sanity/client";
+import { createClient } from "@sanity/client";
 
 //@ts-ignore
 import { getIt } from "get-it";
@@ -16,26 +16,27 @@ const oneSecMail = getIt([
 
 oneSecMail.use(promise({ onlyBody: true }));
 
+let sanityClient = createClient({
+  projectId: "jgnu3d9f",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2021-03-25",
+});
+
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000/",
     async setupNodeEvents(on, config) {
-      // let sanityClient = createClient({
-      //   projectId: "jgnu3d9f",
-      //   dataset: "production",
-      //   useCdn: true,
-      //   apiVersion: "2021-03-25",
-      // });
-      // const pages = await sanityClient.fetch<{ slug: string }[]>(
-      //   `*[_type == 'page'][]{'slug': select( defined(pageType) => '/' + pageType->slug.current + '/'+slug.current,slug.current  )}`
-      // );
-      // const domains = await oneSecMail({ url: "/?action=getDomainList" });
-      // const domain = domains[0];
-      // const name = "test__kreisel__user";
+      const pages = await sanityClient.fetch<{ slug: string }[]>(
+        `*[_type == 'page'][]{'slug': select( defined(pageType) => '/' + pageType->slug.current + '/'+slug.current,slug.current  )}`
+      );
+      const domains = await oneSecMail({ url: "/?action=getDomainList" });
+      const domain = domains[0];
+      const name = "test__kreisel__user";
 
-      // config.env.testMail = { address: `${name}@${domains[0]}`, domain, name };
-      // config.env.pages = pages;
+      config.env.testMail = { address: `${name}@${domains[0]}`, domain, name };
+      config.env.pages = pages;
 
       return config;
     },
@@ -45,24 +46,14 @@ export default defineConfig({
     async setupNodeEvents(on, config) {
       addMatchImageSnapshotPlugin(on, config);
 
-      // let sanityClient = createClient({
-      //   projectId: "jgnu3d9f",
-      //   dataset: "production",
-      //   useCdn: true,
-      //   apiVersion: "2021-03-25",
-      // });
+      const image = await sanityClient.fetch<{ _id: string }>(
+        `*[_type == "sanity.imageAsset"][7]{...}`
+      );
 
-      // const image = await sanityClient.fetch<{ _id: string }>(
-      //   `*[_type == "sanity.imageAsset"][7]{...}`
-      // );
-
-      // config.env.image = image;
-
-      // console.log(image);
-
+      config.env.image = image;
       config.env.imageRef = {
         asset: {
-          _ref: "image-0e0c8f6e2a30b7a6709501851250898d4fbc1100-512x512-png",
+          _ref: image._id,
           _type: "reference",
         },
       };
