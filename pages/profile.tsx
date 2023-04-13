41705ed278ede73c31b81a870a5c16ec1ce71f86
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2023-04-13 15:51:50 +0200

    adding status to profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,3 +1,4 @@
+import { systemText } from "../constants";
 import ProfileForm from "lib/Profile/ProfileForm";
 import {
   fetchProfileData,
@@ -9,6 +10,7 @@ import { Session } from "next-auth";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import React, { ReactElement, ReactNode } from "react";
+import clsx from "clsx";
 
 type ProfileProps = {
   session: Session | null;
@@ -17,13 +19,14 @@ type ProfileProps = {
 const ProfilePage: React.FC<ProfileProps> & {
   getLayout: (page: ReactElement) => ReactNode;
 } = (props) => {
-  const { allowProfile } = props;
+  const { allowProfile, allowMember } = props;
 
   return (
     <div className="w-full items-center justify-center">
-      <div className="bg-primary-light py-24 mb-24 px-5">
-        <div className=" max-w-3xl mx-auto">
-          <h1 className="font-header text-2xl"> Dein Kreisel Profil </h1>
+      <div className="mb-24 bg-primary-light py-24 px-5">
+        <div className=" mx-auto max-w-3xl">
+          <h1 className="font-header text-2xl">Dein Kreisel Profil</h1>
+          <MemberInfo allowed={allowMember} />
         </div>
       </div>
 
@@ -35,7 +38,7 @@ const ProfilePage: React.FC<ProfileProps> & {
 };
 
 ProfilePage.getLayout = function getLayout(page) {
-  return <div className=" ">{page}</div>;
+  return <div>{page}</div>;
 };
 
 // eslint-disable-next-line import/no-unused-modules
@@ -73,3 +76,22 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 // eslint-disable-next-line import/no-unused-modules
 export default ProfilePage;
+
+const MemberInfo = ({ allowed }: { allowed: boolean }) => {
+  return (
+    <div className={clsx("pt-8 text-sm")}>
+      <span
+        className={clsx({
+          "text-green-600": allowed,
+          "text-rose-600": !allowed,
+        })}
+      >
+        {"Status: "}
+        {allowed
+          ? systemText.profile.status.allowed
+          : systemText.profile.status.disallowed}
+      </span>
+      {!allowed && <p>{systemText.profile.status.disallowedDescription}</p>}
+    </div>
+  );
+};
