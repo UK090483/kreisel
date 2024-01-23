367852bf77a423feae30ccae76751abc236bb6d6
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2024-01-23 22:16:43 +0100

    b

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,22 +10,30 @@ let transporter = nodemailer.createTransport({
   },
 });
 
-type Data = {
-  mail: any;
-};
+type Data = any;
 
 // eslint-disable-next-line import/no-unused-modules
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const mail = await transporter.sendMail({
-    from: `"KREISEL e.V." <${process.env.EMAIL_FROM}>`, // sender address
-    to: "konradullrich@me.com",
-    subject: "bli", // Subject line
-    text: "bla", // plain text body
-    //html: "blub,", // html body
-  });
+  if (req.query.send) {
+    const mail = await transporter.sendMail({
+      from: `"KREISEL e.V." <${process.env.EMAIL_FROM}>`, // sender address
+      to: "konradullrich@me.com",
+      subject: "bli", // Subject line
+      text: "bla", // plain text body
+      //html: "blub,", // html body
+    });
+    return res.json({ mail });
+  }
 
-  res.json({ mail });
+  res.json({
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      //pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+  });
 }
